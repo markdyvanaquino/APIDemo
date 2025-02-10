d72ec2967c2e2341b0b5cdc010c82ada26b8dd3a
Mark Dyvan Aquino <markdyvanaquino@gmail.com>
2025-02-10 15:00:00 +0800

    added unit tests for API services

---
+++ Demo.Tests/WebApiTests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*To do: Implement controller tests */
+namespace Demo.Tests
+{
+    [TestClass]
+    public class ProductsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Demo.Tests/WebApiTests/Services/ProductsServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/* To do: implement service tests */
+namespace Demo.Tests
+{
+    [TestClass]
+    public class ProductsServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
